ffc70312402b8090d8335ce1ee89acde9fd2d382
Vineetha Aredla <112147217+VineethaAredla@users.noreply.github.com>
2022-11-03 22:33:16 -0500

    bonus page is added

--- routes/Dog.js
+++ routes/Dog.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+/*
 class Dog {
 constructor(dog_Name, age, price){
   this.dog_Name=dog_Name;
@@ -8,14 +8,14 @@ constructor(dog_Name, age, price){
   this.price=price;
   
 }
-}
+}*/
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let D1=new Dog('Alaskan Malamute',5,12000);
+  /*let D1=new Dog('Alaskan Malamute',5,12000);
   let D2=new Dog('Akita',9,10000);
-  let D3=new Dog('American Leopard Hound',12,13000);
+  let D3=new Dog('American Leopard Hound',12,13000);*/
 
-  res.render('Dog', { title: 'Search Results Dog',Dog : [D1,D2,D3] });
+  res.render('Dog', { title: 'Search Results Dog' });
 });
 
 
